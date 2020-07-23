55e8076013d1cbcc015a9452c10b23d7cd178c9a
Damiandad1 <dpolichowski@edu.cdv.pl>
2020-07-23 19:31:35 +0200

    AI states, Jump, Health, Damage, Skills

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Rigidbody2D _myRb;
+    private Vector2 moveVelocity;
+
+    private void Awake()
+    {
+        _myRb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * _speed;
+    }
+
+    private void FixedUpdate()
+    {
+        _myRb.MovePosition(_myRb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
